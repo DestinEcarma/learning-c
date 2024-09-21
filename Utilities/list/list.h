55b013929e48832c0c292c669b02aef3dc5cc77b
Destin Ecarma <109215379+DestinEcarma@users.noreply.github.com>
2024-09-22 01:32:25 +0800

    Update list.h
    Added explanations to functions.

--- Utilities/list/list.h
+++ Utilities/list/list.h
@@ -11,14 +11,22 @@ typedef struct {
 	size_t size;
 } List;
 
+// Create a new list.
 List newList(int capacity, size_t size);
 
+// Get the length of the list.
 size_t listLength(List *list);
 
+// Get an element from the list from the index.
 void *listGet(List *list, size_t index);
+
+// Set an element in the list from the index.
 bool listSet(List *list, size_t index, void *element);
 
+// Push an element to the list.
 bool listPush(List *list, void *element);
+
+// Pop an element from the list.
 void *listPop(List *list);
 
 #endif
